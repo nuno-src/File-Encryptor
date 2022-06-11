ab1b9159f22e57b0c3c00aed666028c16f5e58db
nuno <75589883+nuno-src@users.noreply.github.com>
2022-06-12 00:16:38 +0100

    Funcional

--- File-Encryptor/Form1.Designer.cs
+++ File-Encryptor/Form1.Designer.cs
@@ -28,173 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button5 = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.buttonBrowseFileDecryptor = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.buttonBrowseKey = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonBrowseFile = new System.Windows.Forms.Button();
+            this.radioButtonEncrypt = new System.Windows.Forms.RadioButton();
+            this.radioButtonDecrypt = new System.Windows.Forms.RadioButton();
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.tabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
-            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tabControl1
+            // buttonStart
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(-1, 0);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(606, 227);
-            this.tabControl1.TabIndex = 0;
-            // 
-            // tabPage1
-            // 
-            this.tabPage1.Controls.Add(this.textBox1);
-            this.tabPage1.Controls.Add(this.button3);
-            this.tabPage1.Controls.Add(this.button1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(598, 201);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "Encryptor";
-            this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(162, 58);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(381, 20);
-            this.textBox1.TabIndex = 2;
-            // 
-            // button3
-            // 
-            this.button3.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(49, 46);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(107, 32);
-            this.button3.TabIndex = 1;
-            this.button3.Text = "Browse File";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(49, 99);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(107, 45);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Encriptar";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Controls.Add(this.listBox1);
-            this.tabPage2.Controls.Add(this.textBox3);
-            this.tabPage2.Controls.Add(this.button5);
-            this.tabPage2.Controls.Add(this.textBox2);
-            this.tabPage2.Controls.Add(this.buttonBrowseFileDecryptor);
-            this.tabPage2.Controls.Add(this.button2);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(598, 201);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Decryptor";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(129, 56);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(381, 20);
-            this.textBox3.TabIndex = 6;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(26, 54);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(97, 23);
-            this.button5.TabIndex = 5;
-            this.button5.Text = "Browse Key File";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.buttonStart.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStart.Location = new System.Drawing.Point(105, 201);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(484, 45);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(129, 16);
+            this.textBox2.Location = new System.Drawing.Point(208, 85);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(381, 20);
-            this.textBox2.TabIndex = 4;
+            this.textBox2.TabIndex = 6;
             // 
-            // buttonBrowseFileDecryptor
+            // buttonBrowseKey
             // 
-            this.buttonBrowseFileDecryptor.Location = new System.Drawing.Point(26, 14);
-            this.buttonBrowseFileDecryptor.Name = "buttonBrowseFileDecryptor";
-            this.buttonBrowseFileDecryptor.Size = new System.Drawing.Size(97, 23);
-            this.buttonBrowseFileDecryptor.TabIndex = 3;
-            this.buttonBrowseFileDecryptor.Text = "Browse File";
-            this.buttonBrowseFileDecryptor.UseVisualStyleBackColor = true;
-            this.buttonBrowseFileDecryptor.Click += new System.EventHandler(this.buttonBrowseFileDecryptor_Click);
+            this.buttonBrowseKey.Location = new System.Drawing.Point(105, 83);
+            this.buttonBrowseKey.Name = "buttonBrowseKey";
+            this.buttonBrowseKey.Size = new System.Drawing.Size(97, 23);
+            this.buttonBrowseKey.TabIndex = 5;
+            this.buttonBrowseKey.Text = "Browse Key File";
+            this.buttonBrowseKey.UseVisualStyleBackColor = true;
+            this.buttonBrowseKey.Click += new System.EventHandler(this.buttonBrowseKey_Click);
             // 
-            // button2
+            // textBox1
             // 
-            this.button2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(37, 148);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(148, 44);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "Desencriptar";
-            this.button2.UseVisualStyleBackColor = true;
+            this.textBox1.Location = new System.Drawing.Point(208, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(381, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // buttonBrowseFile
+            // 
+            this.buttonBrowseFile.Location = new System.Drawing.Point(105, 43);
+            this.buttonBrowseFile.Name = "buttonBrowseFile";
+            this.buttonBrowseFile.Size = new System.Drawing.Size(97, 23);
+            this.buttonBrowseFile.TabIndex = 3;
+            this.buttonBrowseFile.Text = "Browse File";
+            this.buttonBrowseFile.UseVisualStyleBackColor = true;
+            this.buttonBrowseFile.Click += new System.EventHandler(this.buttonBrowseFile_Click);
+            // 
+            // radioButtonEncrypt
+            // 
+            this.radioButtonEncrypt.AutoSize = true;
+            this.radioButtonEncrypt.Location = new System.Drawing.Point(170, 132);
+            this.radioButtonEncrypt.Name = "radioButtonEncrypt";
+            this.radioButtonEncrypt.Size = new System.Drawing.Size(61, 17);
+            this.radioButtonEncrypt.TabIndex = 7;
+            this.radioButtonEncrypt.TabStop = true;
+            this.radioButtonEncrypt.Text = "Encrypt";
+            this.radioButtonEncrypt.UseVisualStyleBackColor = true;
+            this.radioButtonEncrypt.CheckedChanged += new System.EventHandler(this.radioButtonEncrypt_CheckedChanged);
+            // 
+            // radioButtonDecrypt
+            // 
+            this.radioButtonDecrypt.AutoSize = true;
+            this.radioButtonDecrypt.Location = new System.Drawing.Point(411, 132);
+            this.radioButtonDecrypt.Name = "radioButtonDecrypt";
+            this.radioButtonDecrypt.Size = new System.Drawing.Size(62, 17);
+            this.radioButtonDecrypt.TabIndex = 8;
+            this.radioButtonDecrypt.TabStop = true;
+            this.radioButtonDecrypt.Text = "Decrypt";
+            this.radioButtonDecrypt.UseVisualStyleBackColor = true;
+            this.radioButtonDecrypt.CheckedChanged += new System.EventHandler(this.radioButtonDecrypt_CheckedChanged);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(381, 93);
+            this.listBox1.Location = new System.Drawing.Point(612, 276);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(120, 95);
-            this.listBox1.TabIndex = 7;
+            this.listBox1.TabIndex = 9;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(602, 226);
-            this.Controls.Add(this.tabControl1);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(772, 395);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.radioButtonDecrypt);
+            this.Controls.Add(this.radioButtonEncrypt);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.buttonBrowseFile);
+            this.Controls.Add(this.buttonBrowseKey);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "Form1";
-            this.tabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
-            this.tabPage1.PerformLayout();
-            this.tabPage2.ResumeLayout(false);
-            this.tabPage2.PerformLayout();
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button buttonStart;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonBrowseFile;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button buttonBrowseFileDecryptor;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button buttonBrowseKey;
+        private System.Windows.Forms.RadioButton radioButtonEncrypt;
+        private System.Windows.Forms.RadioButton radioButtonDecrypt;
         private System.Windows.Forms.ListBox listBox1;
     }
 }
--- File-Encryptor/Form1.cs
+++ File-Encryptor/Form1.cs
@@ -19,78 +19,49 @@ namespace File_Encryptor
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            radioButtonEncrypt.Checked = true;
+            listBox1.Visible = false;
 
-        //browse file tab 1
-        private void button3_Click(object sender, EventArgs e)
+        }
+
+        private void radioButtonEncrypt_CheckedChanged(object sender, EventArgs e)
+        {
+
+
+            buttonBrowseKey.Enabled = false;
+            buttonBrowseKey.Visible = false;
+            textBox2.Enabled = false;
+            textBox2.Visible = false;
+
+
+        }
+
+        private void radioButtonDecrypt_CheckedChanged(object sender, EventArgs e)
+        {
+
+            buttonBrowseKey.Enabled = true;
+            buttonBrowseKey.Visible = true;
+            textBox2.Enabled = true;
+            textBox2.Visible = true;
+
+        }
+
+
+
+        private void buttonBrowseFile_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
-            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            if (ofd.ShowDialog() == DialogResult.OK)
             {
                 textBox1.Text = ofd.FileName;
             }
+            
         }
 
-        // tab1 encriptar
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (File.Exists(textBox1.Text))
-            {
-
-
-                string filepath = textBox1.Text;
-
-                //string[] lines = File.ReadAllLines(filepath);
-
-                //string linha = sr.ReadLine();
-
-                List<string> lines = new List<string>();
-                lines = File.ReadAllLines(filepath).ToList();
-
-
-
-                //foreach(string line in lines)
-                //{
-                //    listBox1.Items.Add(line);
-                //}
-
-
-
-
-                FileStream fs = File.Create(textBox1.Text);
-                //AesCryptoServiceProvider cod = new AesCryptoServiceProvider();
-
-                Aes cod = Aes.Create();
-                CryptoStream cr = new CryptoStream(fs, cod.CreateEncryptor(), CryptoStreamMode.Write);
-
-                StreamWriter sw = new StreamWriter(cr);
-
-                foreach (string line in lines)
-                {
-                    sw.WriteLine(line);
-                }
-
-                sw.Close();
-                fs.Close();
-
-                FileStream fsChave = File.Create("chave.key");
-
-                BinaryWriter bw = new BinaryWriter(fsChave);
-
-                bw.Write(cod.Key);
-                bw.Write(cod.IV);
-                bw.Close();
-                fsChave.Close();
-
-                MessageBox.Show("Ficheiro encriptado com sucesso.");
-
-            }
-        }
-    
-        
-
-
-        //tab2 browse file
-        private void buttonBrowseFileDecryptor_Click(object sender, EventArgs e)
+        // browse key file
+        private void buttonBrowseKey_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
             if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
@@ -100,90 +71,273 @@ namespace File_Encryptor
         }
 
 
-        // tab 2 browse key file
-        private void button5_Click(object sender, EventArgs e)
+        private void buttonStart_Click(object sender, EventArgs e)
         {
-            OpenFileDialog ofd = new OpenFileDialog();
-            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            if (radioButtonEncrypt.Checked == true)
             {
-                textBox3.Text = ofd.FileName;
-            }
-        }
 
-
-        // tab2 desencriptar
-        private void button2_Click(object sender, EventArgs e)
-        {
-            if (File.Exists(textBox2.Text))
-            {
-                FileStream fich = File.OpenRead(textBox2.Text);
-                FileStream chave = File.OpenRead(textBox3.Text);
-
-                string filepath = textBox2.Text;
-
-                //AesCryptoServiceProvider cod = new AesCryptoServiceProvider();
-                Aes cod = Aes.Create();
-                BinaryReader brKey = new BinaryReader(chave);
-                cod.Key = brKey.ReadBytes(32);
-                cod.IV = brKey.ReadBytes(16);
-
-
-                CryptoStream cs = new CryptoStream(fich, cod.CreateDecryptor(), CryptoStreamMode.Read);
-
-                StreamReader sr = new StreamReader(cs);
-
-
-                List<string> lines = new List<string>();
-                //string[] aux;
-
-                string linha = sr.ReadLine();
-
-                while (linha != null)
+                if (File.Exists(textBox1.Text))
                 {
 
-                    lines.Add(linha);
 
-                    linha = sr.ReadLine();
+                    string filepath = textBox1.Text;
+
+                    //string[] lines = File.ReadAllLines(filepath);
+
+                    //string linha = sr.ReadLine();
+
+                    List<string> lines = new List<string>();
+                    lines = File.ReadAllLines(filepath).ToList();
+
+
+
+                    //foreach(string line in lines)
+                    //{
+                    //    listBox1.Items.Add(line);
+                    //}
+
+
+
+
+                    FileStream fs = File.Create(textBox1.Text);
+                    //AesCryptoServiceProvider cod = new AesCryptoServiceProvider();
+
+                    Aes cod = Aes.Create();
+                    CryptoStream cr = new CryptoStream(fs, cod.CreateEncryptor(), CryptoStreamMode.Write);
+
+                    StreamWriter sw = new StreamWriter(cr);
+
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line);
+                    }
+
+                    sw.Close();
+                    fs.Close();
+
+                    FileStream fsChave = File.Create("chave.key");
+
+                    BinaryWriter bw = new BinaryWriter(fsChave);
+
+                    bw.Write(cod.Key);
+                    bw.Write(cod.IV);
+                    bw.Close();
+                    fsChave.Close();
+
+                    MessageBox.Show("Ficheiro encriptado com sucesso.");
                 }
-
-
-                //string filepath = textBox2.Text;
-
-                //string[] lines = File.ReadAllLines(filepath);
-
-                //string linha = sr.ReadLine();
-
-                // List<string> lines = new List<string>();
-                //lines = File.ReadAllLines(filepath).ToList();
-
-                sr.Close();
-                fich.Close();
-                chave.Close();
-
-                foreach (string line in lines)
-                {
-                    listBox1.Items.Add(line);
-                    //File.WriteAllLines(filepath, line);
-                }
-
-
-
-
-
-
-                //foreach (string line in lines)
-                //{
-                //    File.WriteAllLines(filepath,lines);
-                //}
-
-
-               
-
-
-                MessageBox.Show("Ficheiro desencriptado com sucesso.");
 
             }
+            else if (radioButtonDecrypt.Checked == true)
+            {
+                if (File.Exists(textBox1.Text) && File.Exists(textBox2.Text))
+                {
+                    
+                    string filepath = textBox1.Text;
+                    string filepath_key = textBox2.Text;
+
+                    FileStream fich = File.OpenRead(filepath);
+                    FileStream chave = File.OpenRead(filepath_key);
+
+
+
+                    //AesCryptoServiceProvider cod = new AesCryptoServiceProvider();
+                    Aes cod = Aes.Create();
+                    BinaryReader brKey = new BinaryReader(chave);
+                    cod.Key = brKey.ReadBytes(32);
+                    cod.IV = brKey.ReadBytes(16);
+
+
+                    CryptoStream cs = new CryptoStream(fich, cod.CreateDecryptor(), CryptoStreamMode.Read);
+
+                    StreamReader sr = new StreamReader(cs);
+
+
+                    //List<string> lines; // = File.ReadAllLines(sr).ToList();//new List<string>();
+                    //string[] aux;
+
+                    string linha = sr.ReadLine();
+                    while (linha != null)
+                    {
+                        
+                        listBox1.Items.Add(linha);
+                        
+                        //lines.Append(linha);
+                        linha = sr.ReadLine();
+                    }
+
+                    sr.Close();
+                    fich.Close();
+                    chave.Close();
+
+                    StreamWriter sw = new StreamWriter(filepath);
+
+                    foreach(string i in listBox1.Items)
+                    {
+                        sw.WriteLine(i);
+                    }
+
+                    sw.Close();
+
+                    //while (linha != null)
+                    //{
+                    //    string[] aux = linha.Split('.');
+                    //    lines.Add(linha);
+                    //    listBox1.Items.Add(aux);
+
+                    //    linha = sr.ReadLine();
+                    //}
+
+
+                    //string filepath = textBox2.Text;
+
+                    //string[] lines = File.ReadAllLines(filepath);
+
+                    //string linha = sr.ReadLine();
+
+                    // List<string> lines = new List<string>();
+                    //lines = File.ReadAllLines(filepath).ToList();
+
+                    sr.Close();
+                    fich.Close();
+                    chave.Close();
+
+                    //foreach (string line in lines)
+                    //{
+                    //    listBox1.Items.Add(line);
+                    //    //File.WriteAllLines(filepath, line);
+                    //}
+
+
+
+
+
+
+                    //foreach (string line in lines)
+                    //{
+                    //    File.WriteAllLines(filepath,lines);
+                    //}
+
+
+
+
+
+                    MessageBox.Show("Ficheiro desencriptado com sucesso.");
+
+                    
+                }
+            }
+
+
+
+
+            //tab2 browse file
+            //private void buttonBrowseFileDecryptor_Click(object sender, EventArgs e)
+            //{
+            //    OpenFileDialog ofd = new OpenFileDialog();
+            //    if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            //    {
+            //        textBox1.Text = ofd.FileName;
+            //    }
+            //}
+
+
+            // tab 2 browse key file
+            //private void button5_Click(object sender, EventArgs e)
+            //{
+            //    OpenFileDialog ofd = new OpenFileDialog();
+            //    if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            //    {
+            //        textBox2.Text = ofd.FileName;
+            //    }
+            //}
+
+
+            //// tab2 desencriptar
+            //private void button2_Click(object sender, EventArgs e)
+            //{
+            //    if (File.Exists(textBox1.Text))
+            //    {
+            //        string filepath = textBox1.Text;
+            //        string filepath_key = textBox2.Text;
+
+            //        FileStream fich = File.OpenRead(filepath);
+            //        FileStream chave = File.OpenRead(filepath_key);
+
+
+
+            //        //AesCryptoServiceProvider cod = new AesCryptoServiceProvider();
+            //        Aes cod = Aes.Create();
+            //        BinaryReader brKey = new BinaryReader(chave);
+            //        cod.Key = brKey.ReadBytes(32);
+            //        cod.IV = brKey.ReadBytes(16);
+
+
+            //        CryptoStream cs = new CryptoStream(fich, cod.CreateDecryptor(), CryptoStreamMode.Read);
+
+            //        StreamReader sr = new StreamReader(cs);
+
+
+            //        //List<string> lines = File.ReadAllLines(sr).ToList();//new List<string>();
+            //        //string[] aux;
+
+            //        string linha;
+            //        do
+            //        {
+            //            linha = sr.ReadLine();
+            //            listBox1.Items.Add(linha);
+            //        }while (linha != null);
+
+            //        //while (linha != null)
+            //        //{
+            //        //    string[] aux = linha.Split('.');
+            //        //    lines.Add(linha);
+            //        //    listBox1.Items.Add(aux);
+
+            //        //    linha = sr.ReadLine();
+            //        //}
+
+
+            //        //string filepath = textBox2.Text;
+
+            //        //string[] lines = File.ReadAllLines(filepath);
+
+            //        //string linha = sr.ReadLine();
+
+            //        // List<string> lines = new List<string>();
+            //        //lines = File.ReadAllLines(filepath).ToList();
+
+            //        sr.Close();
+            //        fich.Close();
+            //        chave.Close();
+
+            //        //foreach (string line in lines)
+            //        //{
+            //        //    listBox1.Items.Add(line);
+            //        //    //File.WriteAllLines(filepath, line);
+            //        //}
+
+
+
+
+
+
+            //        //foreach (string line in lines)
+            //        //{
+            //        //    File.WriteAllLines(filepath,lines);
+            //        //}
+
+
+
+
+
+            //        MessageBox.Show("Ficheiro desencriptado com sucesso.");
+
+            //    }
+
+
         }
 
+        
     }
 }
